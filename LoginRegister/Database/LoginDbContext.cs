6e3d6e7ca276a07b2de2173dbc247d9498e754b6
grbzemir <eyetkingurbuz@trakya.edu.tr>
2025-03-03 22:41:54 +0300

    Migration

---
+++ LoginRegister/Database/LoginDbContext.cs
@@ -0,0 +1,15 @@
+﻿using LoginRegister.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LoginRegister.Database
+{
+    public class LoginDbContext:DbContext
+    {
+        public LoginDbContext(DbContextOptions<LoginDbContext>options):base(options) 
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
