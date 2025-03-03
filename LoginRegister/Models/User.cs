13b061b53c85ec63edfe7e828af15607e574b4b7
grbzemir <eyetkingurbuz@trakya.edu.tr>
2025-03-03 22:28:29 +0300

    Entities

--- LoginRegister/Models/User.cs
+++ LoginRegister/Models/User.cs
@@ -7,6 +7,6 @@
         public string LastName { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-        public int IsActive { get; set; }
+        public int IsActive { get; set; } = 1;
     }
 }
